816b3d3043de6b0215451dc0f71c0450a6c3942b
Damian Krupa <damian.krupa@sabre.com>
2021-01-30 17:44:35 +0100

    correct team section styling

--- src/components/TeamSection/TeamSection.js
+++ src/components/TeamSection/TeamSection.js
@@ -32,13 +32,13 @@ const TeamSection = () => {
       <div className="row justify-content-center m-5">
         <TitleBadge value="zespół" />
       </div>
-      <section className="row">
-        <div className="col-4 p-4">
+      <section className="row justify-content-center">
+        <div className="col-6 col-lg-4 p-4">
           <Img
             fluid={data.allPhotosJson.edges[0].node.src.childImageSharp.fluid}
           />
         </div>
-        <article className="col-8">
+        <article className="col-12 col-lg-8">
           <h1 id="about-us">Magdalena Gospodarek</h1>
           <h3>CEO & Founder</h3>
           <p>
@@ -57,25 +57,33 @@ const TeamSection = () => {
         </article>
       </section>
       <section className="row justify-content-end mb-5">
-        <div className="col-2">
+        <div className="col-3 col-lg-2 text-center">
           <Img
             fluid={data.allPhotosJson.edges[1].node.src.childImageSharp.fluid}
           />
+          <h5>Adam Rus</h5>
+          <h6>Projektant graficzny</h6>
         </div>
-        <div className="col-2">
+        <div className="col-3 col-lg-2 text-center" >
           <Img
             fluid={data.allPhotosJson.edges[2].node.src.childImageSharp.fluid}
           />
+          <h5>Izabela Antczak</h5>
+          <h6>Tłumacz j. niemieckiego</h6>
         </div>
-        <div className="col-2">
+        <div className="col-3 col-lg-2 text-center" >
           <Img
             fluid={data.allPhotosJson.edges[3].node.src.childImageSharp.fluid}
           />
+          <h5>Hubert Krakowiak</h5>
+          <h6>Operator kamery</h6>
         </div>
-        <div className="col-2">
+        <div className="col-3 col-lg-2 text-center" >
           <Img
             fluid={data.allPhotosJson.edges[4].node.src.childImageSharp.fluid}
           />
+          <h5>Paulina Mania</h5>
+          <h6>Fotograf</h6>
         </div>
       </section>
     </>
