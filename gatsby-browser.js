0eca7d5400d0030036a89aca03b72ab36701f6a4
Damian Krupa <damian.k.krupa@gmail.com>
2021-07-23 16:21:25 +0200

    fix fullcall widget

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react")
+
+exports.onInitialClientRender = () => {
+  localStorage.setItem("fcWebHash", "004e7dcdf427d3fe5357686a01ac212cb56c49c7601dd236fe5e462122b87c37");
+  const fc = document.createElement('script');
+  fc.type="text/javascript";
+  fc.async=true;
+  fc.src = "https://app.fullcall.com/api/static/core/004e7dcdf427d3fe5357686a01ac212cb56c49c7601dd236fe5e462122b87c37";
+  const s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(fc,s);
+}
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,11 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-const React = require("react")
-exports.onRenderBody = ({
-                          setPostBodyComponents,
-                        }) => {
-  setPostBodyComponents([
-    <script type="text/javascript" async src="https://app.fullcall.com/api/static/core/004e7dcdf427d3fe5357686a01ac212cb56c49c7601dd236fe5e462122b87c37" />
-  ])
-}
