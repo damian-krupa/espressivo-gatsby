d7b42bbbcf669b8660d4650b542ceda4550f02b6
Damian Krupa <damian.k.krupa@gmail.com>
2021-08-16 13:35:40 +0200

    add facebook meta

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,10 +4,16 @@ import "assets/styles/global-styles.scss"
 import { Container } from "react-bootstrap"
 import Navigation from "components/Navigation/Navigation"
 import Footer from "../components/Footer/Footer"
+import { Helmet } from "react-helmet"
 
 const MainLayout = ({ children }) => {
   return (
     <div className="layout">
+      <Helmet>
+        <html lang="pl-PL" />
+        <title>Espressivo</title>
+        <meta name="facebook-domain-verification" content="typ6m312ajt31eizky04rsx1gwp8sv"/>
+      </Helmet>
       <div id="top" />
       <TopBar />
       <Navigation />
