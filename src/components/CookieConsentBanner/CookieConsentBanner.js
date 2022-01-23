65da582f4a9a63c882074c0ab0d75ea4dbd2ee15
Damian Krupa <damian.k.krupa@gmail.com>
2022-01-23 23:45:50 +0100

    cookie banner styling

--- src/components/CookieConsentBanner/CookieConsentBanner.js
+++ src/components/CookieConsentBanner/CookieConsentBanner.js
@@ -4,10 +4,10 @@ import CookieConsent from "react-cookie-consent"
 const CookieConsentBanner = () => {
   return (
     <CookieConsent
-      style={{ fontSize: "0.7rem", padding: "0 7rem", zIndex: "1001" }}
+      style={{ fontSize: "0.7rem", padding: "0 7rem", zIndex: "1001", justifyContent: "center" }}
       buttonClasses="btn btn-primary"
       buttonText="Zgoda"
-      contentStyle={{ flex: "1 0" }}
+      contentStyle={{ flex: "1 0", textAlign: "center" }}
     >
       Ta strona korzysta z ciasteczek, aby świadczyć usługi na najwyższym
       poziomie. Dalsze korzystanie ze strony oznacza, że zgadzasz się na ich
