f42185e056db87d114ad30f72b24f46419be4afc
damian_krupa <damian.krupa.ctr@sabre.com>
2022-10-26 21:26:26 +0200

    add google gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,27 @@ module.exports = {
         path: `./src/data/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["G-VG88M8VXZ7"],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          // optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
     "gatsby-plugin-optimize-svgs",
     "gatsby-plugin-scroll-reveal",
     // this (optional) plugin enables Progressive Web App + Offline functionality
